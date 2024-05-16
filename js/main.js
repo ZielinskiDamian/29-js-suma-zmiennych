03934ebaf26913f97b1e917e7597280b171233b8
damian zielinski <dazielinscy@gmail.com>
2024-05-16 08:47:13 +0200

    zmiany

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let firstNumber = 1;
+let secondNumber = 2;
+function resolut(firstNumber, secondNumber) {
+	let resolut = firstNumber + secondNumber;
+	return resolut;
+}
+console.log(resolut(3,7));
